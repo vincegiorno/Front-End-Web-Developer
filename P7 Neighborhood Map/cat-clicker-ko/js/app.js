cc6062ef851995e2147ad306e86a8749dc81ec1a
李华阳Martian <martian2lee@163.com>
2017-08-01 18:07:07 +0800

    error

--- P7 Neighborhood Map/cat-clicker-ko/js/app.js
+++ P7 Neighborhood Map/cat-clicker-ko/js/app.js
@@ -1,9 +1,47 @@
-var Cat = function () {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('https://www.flickr.com/photos/bigtallguy/434164568');
-    this.nicknames = ko.observableArray(['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha Tab Tabby Catty Cat']);
+var initialCats = [
+    {
+        clickCount : 0,
+        name : 'Tabby',
+        imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+        nicknames: ['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha Tab Tabby Catty Cat']
+    },
+    {
+        clickCount : 0,
+        name : 'Tiger',
+        imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+        nicknames: ['Tigger']
+    },
+    {
+        clickCount : 0,
+        name : 'Scaredy',
+        imgSrc : 'img/22252709_010df3379e_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+        nicknames: ['Casper']
+    },
+    {
+        clickCount : 0,
+        name : 'Shadow',
+        imgSrc : 'img/1413379559_412a540d29_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+        nicknames: ['Shooby']
+    },
+    {
+        clickCount : 0,
+        name : 'Sleepy',
+        imgSrc : 'img/9648464288_2516b35537_z.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+        nicknames: ['Zzzzz']
+    }
+];
+
+var Cat = function (data) {
+    this.clickCount = data.clickCount;
+    this.name = data.name;
+    this.imgSrc = data.imgSrc;
+    this.imgAttribution = data.imgAttribution;
+    this.nicknames = data.nicknames;
 
     this.title = ko.computed(function () {
         var title;
@@ -29,7 +67,13 @@ var Cat = function () {
 var ViewModel = function () {
     var self = this;
 
-    this.currentCat = ko.observable(new Cat());
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function (catItem) {
+        self.catList.push( new Cat(catItem) );
+    });
+
+    this.currentCat = ko.observable( this.catList()[0] );
 
     this.incrementCounter = function () {
       self.currentCat().clickCount(self.currentCat().clickCount() + 1);
