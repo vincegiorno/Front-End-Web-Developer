0ed5a0cb0766671c63c53755c6cc2af353271cf6
李华阳Martian <martian2lee@163.com>
2017-08-01 11:40:27 +0800

    cat-clicker final version

--- P7 Neighborhood Map/cat-clicker/js/app.js
+++ P7 Neighborhood Map/cat-clicker/js/app.js
@@ -19,41 +19,62 @@ $(function () {
     ];
 
     var octopus = {
-        getCat: function (cat) {
-            
+        countCat: function (catName, catCount) {
+            for (var i = 0; i < model.length; i++) {
+                if (catName === model[i].name) {
+                    model[i].clickCount += 1;
+                    viewDisplay.render(catName);
+                    return;
+                }
+            }
         },
 
         init: function () {
-            viewList.render();
             viewDisplay.init();
+            viewList.render();
+            viewList.init();
         }
     };
 
     var viewList = {
+        init: function () {
+            this.$nameList.on('click', '.singleCat', function () {
+                viewDisplay.render($(this).html());
+            });
+        },
+
         render: function () {
             this.$nameList = $('#nameList');
-            var $nameList = this.$nameList,
-                nameTemplate = $('script[data-template="name"]').html(),
+            var nameTemplate = $('script[data-template="name"]').html(),
                 singleCat;
 
             model.forEach(function (cat) {
                singleCat = nameTemplate.replace(/{{name}}/g, cat.name);
-               $nameList.append(singleCat);
+               viewList.$nameList.append(singleCat);
             });
         }
     };
 
     var viewDisplay = {
         init: function () {
-            viewList.$nameList.on('click', '.singleCat', function () {
-                octopus.getCat($(this).html());
+            this.$name = $('#name');
+            this.$image = $('#image');
+            this.$count = $('#count');
+
+            this.$image.click(function () {
+                octopus.countCat(viewDisplay.$name.html());
             });
-            
-            this.render();
         },
-        
-        render: function () {
-            
+
+        render: function (catName) {
+            for (var i = 0; i < model.length; i++) {
+                if (catName === model[i].name) {
+                    viewDisplay.$name.html(catName);
+                    viewDisplay.$image.attr('src', model[i].url);
+                    viewDisplay.$count.html(model[i].clickCount);
+                    return;
+                }
+            }
         }
     };
 
