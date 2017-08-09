913135c8842d568cecf329af39f39957ad6e54fc
李华阳Martian <martian2lee@163.com>
2017-08-09 15:56:21 +0800

    search success!

--- P7 Neighborhood Map/project/js/app.js
+++ P7 Neighborhood Map/project/js/app.js
@@ -1,3 +1,7 @@
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+
 // This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
@@ -6,7 +10,7 @@ var map;
 var infowindow;
 var markers = [];
 
-function initMap() {
+function initAutocomplete() {
     "use strict";
     var newYork = {lat: 40.7413549, lng: -73.9980244};
 
@@ -15,49 +19,72 @@ function initMap() {
         zoom: 13
     });
 
-    infowindow = new google.maps.InfoWindow();
-    var bounds = new google.maps.LatLngBounds();
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-        location: newYork,
-        radius: 500,
-        type: ['restaurant']
-    }, function (results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                var place = results[i];
-                var placeLoc = place.geometry.location;
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-                console.log(placeLoc.lat(), placeLoc.lng());
-
-                var marker = new google.maps.Marker({
-                    map: map,
-                    position: placeLoc,
-                    animation: null
-                });
-                markers.push(marker);
-                bounds.extend(marker.position);
-
-                google.maps.event.addListener(marker, 'click', (function(markerCopy, place) {
-                    return function () {
-                        infowindow.setContent(place.name);
-                        infowindow.open(map, this);
-                        if (markerCopy.getAnimation() !== null) {
-                            markerCopy.setAnimation(null);
-                        } else {
-                            markerCopy.setAnimation(google.maps.Animation.BOUNCE);
-                            setTimeout(function(){ markerCopy.setAnimation(null); }, 750);
-                        }
-                    };
-                })(marker, place));
-            map.fitBounds(bounds);
-            }
-        }
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
     });
 
-    var url = 'https://developers.zomato.com/api/v2.1/search?entity_id=16774318&apikey=8e563edbe434185a64f3948dad0864a8';
+    var infowindow;
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
 
-    $.getJSON(url, function (data) {
-        console.log(data);
+        if (places.length === 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        infowindow = new google.maps.InfoWindow();
+        var bounds = new google.maps.LatLngBounds();
+        var service = new google.maps.places.PlacesService(map);
+        places.forEach(function(place) {
+            service.nearbySearch({
+                location: {lat: place.geometry.location.lat(), lng: place.geometry.location.lng()},
+                radius: 500,
+                type: ['restaurant']
+            }, function (results, status) {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    for (var i = 0; i < results.length; i++) {
+                        var place = results[i];
+                        var placeLoc = place.geometry.location;
+
+                        var marker = new google.maps.Marker({
+                            map: map,
+                            position: placeLoc,
+                            animation: null
+                        });
+                        markers.push(marker);
+                        bounds.extend(marker.position);
+
+                        google.maps.event.addListener(marker, 'click', (function(markerCopy, place) {
+                            return function () {
+                                infowindow.setContent(place.name);
+                                infowindow.open(map, this);
+                                if (markerCopy.getAnimation() !== null) {
+                                    markerCopy.setAnimation(null);
+                                } else {
+                                    markerCopy.setAnimation(google.maps.Animation.BOUNCE);
+                                    setTimeout(function(){ markerCopy.setAnimation(null); }, 750);
+                                }
+                            };
+                        })(marker, place));
+                        map.fitBounds(bounds);
+                    }
+                }
+            });
+        });
     });
 }
