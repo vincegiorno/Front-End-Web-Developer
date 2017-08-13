516b8ea4f3aa44a627855c8bc389294a5375eb43
李华阳Martian <martian2lee@163.com>
2017-08-13 21:10:03 +0800

    first submission

---
+++ P7 Neighborhood Map/project/README.md
@@ -0,0 +1,3 @@
+# Neighborhood restaurant
+
+Restaurant content powered by Zomato APIs, when detailed information about a restaurant cannot be found on Zomato, only restaurant name is showed, and *Select your favorite cuisine* function is not available.
--- P7 Neighborhood Map/project/js/app.js
+++ P7 Neighborhood Map/project/js/app.js
@@ -10,14 +10,21 @@ var map;
 var infowindow;
 var markers = [];
 
-
 function initAutocomplete() {
     "use strict";
     var newYork = {lat: 40.7413549, lng: -73.9980244};
 
+    // New York as initial viewport
     map = new google.maps.Map(document.getElementById('map'), {
         center: newYork,
         zoom: 13
+    });
+
+    // This autocomplete is for use in the geocoder entry box.
+    var zoomAutocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('zoom-to-area-text'));
+    document.getElementById('zoom-to-area').addEventListener('click', function() {
+        zoomToArea();
     });
 
     // Create the search box and link it to the UI element.
@@ -25,24 +32,15 @@ function initAutocomplete() {
     var searchBox = new google.maps.places.SearchBox(input);
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-    // This autocomplete is for use in the geocoder entry box.
-    var zoomAutocomplete = new google.maps.places.Autocomplete(
-        document.getElementById('zoom-to-area-text'));
-
     // Bias the SearchBox results towards current map's viewport.
     map.addListener('bounds_changed', function() {
         searchBox.setBounds(map.getBounds());
-    });
-
-    document.getElementById('zoom-to-area').addEventListener('click', function() {
-        zoomToArea();
     });
 
     // Listen for the event fired when the user selects a prediction and retrieve
     // more details for that place.
     searchBox.addListener('places_changed', function() {
         var places = searchBox.getPlaces();
-
         if (places.length === 0) {
             return;
         }
@@ -52,8 +50,13 @@ function initAutocomplete() {
             marker.setMap(null);
         });
         markers = [];
+
+        // Clear out sidebar
         VM.restaurantList([]);
-        // VM.cuisinesList([]);
+
+        // Clear out dropdown
+        VM.cuisinesList([]);
+        VM.cuisinesUnique = [];
 
         // For each place, get the icon, name and location.
         infowindow = new google.maps.InfoWindow();
@@ -63,7 +66,7 @@ function initAutocomplete() {
             service.nearbySearch({
                 location: {lat: place.geometry.location.lat(), lng: place.geometry.location.lng()},
                 radius: 500,
-                type: ['restaurant']
+                uniqueCuisine: ['restaurant']
             }, function (results, status) {
                 if (status === google.maps.places.PlacesServiceStatus.OK) {
                     for (var i = 0; i < results.length; i++) {
@@ -76,7 +79,6 @@ function initAutocomplete() {
                         });
                         markers.push(marker);
                         bounds.extend(marker.position);
-
                         apiRequest(marker);
                     }
                     map.fitBounds(bounds);
@@ -86,15 +88,16 @@ function initAutocomplete() {
     });
 }
 
-// This function takes the input value in the find nearby area text input
-// locates it, and then zooms into that area. This is so that the user can
-// show all listings, then decide to focus on one area of the map.
+// Go to selected city when 'Go' is clicked
 function zoomToArea() {
     "use strict";
+
     // Initialize the geocoder.
     var geocoder = new google.maps.Geocoder();
+
     // Get the address or place that the user entered.
     var address = document.getElementById('zoom-to-area-text').value;
+
     // Make sure the address isn't blank.
     if (address === '') {
         window.alert('You must enter an area, or address.');
@@ -115,11 +118,13 @@ function zoomToArea() {
     }
 }
 
+// Request data from zomato API
 function apiRequest(marker) {
     "use strict";
     var restaurantAPI = `https://developers.zomato.com/api/v2.1/search?q=${marker.name}&lat=${marker.position.lat()}&lon=${marker.position.lng()}&apikey=fdab76b655596f02ba656c39adca693e`;
     $.getJSON(restaurantAPI, function (data) {
         if (data.results_shown === 0) {
+            // No restaurant info
             marker.api = false;
         } else {
             marker.api = true;
@@ -136,20 +141,21 @@ function apiRequest(marker) {
             getPlacesDetails(this, infowindow);
         });
 
+        // Add marker to restaurantList and cuisinesList for sidebar and dropdown respectively
         VM.addMarker(marker, infowindow);
     }).error(function (e) {
         marker.api = false;
-
         marker.addListener('click', function() {
             getPlacesDetails(this, infowindow);
         });
-
         VM.addMarker(marker, infowindow);
     });
 }
 
+// Populate the DOM
 function getPlacesDetails(marker, infowindow) {
     "use strict";
+
     // Set the marker property on this infowindow so it isn't created again.
     infowindow.marker = marker;
     if (marker.api === true) {
@@ -184,6 +190,7 @@ function getPlacesDetails(marker, infowindow) {
     animation(marker, infowindow);
 }
 
+// Open the infowindow and make the marker bounce once
 function animation(marker, infowindow) {
     "use strict";
     infowindow.open(map, marker);
@@ -198,23 +205,24 @@ function animation(marker, infowindow) {
 var ViewModel = function () {
     "use strict";
     var self = this;
-
     this.restaurantList = ko.observableArray([]);
     this.cuisinesList = ko.observableArray([]);
 
+    // For filtering distinct cuisines
     this.cuisinesUnique = [];
-
     this.addMarker = function (marker, infowindow) {
+        // Add marker to restaurantList for sidebar
         self.restaurantList.push(new Restaurant(marker, infowindow));
 
+        // Add marker to cuisinesList for dropdown
         if (marker.api === true && marker.cuisines !== '') {
             var cuisines = marker.cuisines;
             var cuisinesSplit = cuisines.split(', ');
             cuisinesSplit.forEach(function (cuisine) {
-                var type = new Cuisines(cuisine);
-                if (self.cuisinesUnique.indexOf(type.name) === -1) {
-                    self.cuisinesUnique.push(type.name);
-                    self.cuisinesList.push(type);
+                var uniqueCuisine = new Cuisines(cuisine);
+                if (self.cuisinesUnique.indexOf(uniqueCuisine.name) === -1) {
+                    self.cuisinesUnique.push(uniqueCuisine.name);
+                    self.cuisinesList.push(uniqueCuisine);
                 }
             });
         }
@@ -224,7 +232,11 @@ var ViewModel = function () {
 var Restaurant = function (marker, infowindow) {
     "use strict";
     this.marker = marker;
+
+    // Hide filtered restaurant on sidebar if false
     this.visibility = ko.observable(true);
+
+    // Get infowindow when click on restaurant on sidebar
     this.detail = function () {
         getPlacesDetails(marker, infowindow);
     };
@@ -233,13 +245,13 @@ var Restaurant = function (marker, infowindow) {
 var Cuisines = function (cuisine) {
     "use strict";
     var self = this;
-
     this.name = cuisine;
     this.filter = function () {
         filter(self.name);
     };
 };
 
+// Dropdown filter
 function filter(name) {
     ko.utils.arrayForEach(VM.restaurantList(), function (restaurant) {
         restaurant.marker.setMap(map);
@@ -247,7 +259,10 @@ function filter(name) {
         var cuisines = restaurant.marker.cuisines;
         var cuisinesSplit = cuisines.split(', ');
         if (cuisinesSplit.indexOf(name) === -1) {
+            // Visibility on map
             restaurant.marker.setMap(null);
+
+            // Visibility on sidebar
             restaurant.visibility(false);
         }
     });
